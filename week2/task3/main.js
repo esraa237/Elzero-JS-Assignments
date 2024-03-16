b8688c2060a92c4c2158015581c910383c998dc1
esraa237 <esraafoda2000@gmail.com>
2024-03-16 20:50:35 +0200

    finish task3 in week2

---
+++ week2/task3/main.js
@@ -0,0 +1,8 @@
+
+console.log("`I'm In\n\\\\ \nLove \\\\ \"\"\" '''\n++ With ++\n\\\"\"\"\\\"\"\"\n\"\"JavaScript\"\"``");
+// `I'm In
+// \\
+// Love \\ """ '''
+// ++ With ++
+// \"""\"""
+// ""JavaScript""``
