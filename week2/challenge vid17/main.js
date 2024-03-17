3984a4796c3cac98ae85aeb53601a86d4727ed01
esraa237 <esraafoda2000@gmail.com>
2024-03-17 18:17:14 +0200

    update the structure in week2

--- week2/challenge vid17/main.js
+++
@@ -1,9 +0,0 @@
-let Title = "Elzero", Description = "Elzero Web School",Date = "25/10";
-let Card = `
-<div class="card">
-<h3>Hello ${Title}</h3>
-<p>${Description}</p>
-<span>${Date}</span>
-</div>
-`;
-document.write(Card.repeat(4));
--- week2/task3/main.js
+++
@@ -1,8 +0,0 @@
-
-console.log("`I'm In\n\\\\ \nLove \\\\ \"\"\" '''\n++ With ++\n\\\"\"\"\\\"\"\"\n\"\"JavaScript\"\"``");
-// `I'm In
-// \\
-// Love \\ """ '''
-// ++ With ++
-// \"""\"""
-// ""JavaScript""``
