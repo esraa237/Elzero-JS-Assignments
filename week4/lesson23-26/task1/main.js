bdbe9eb68e8d62e344d82d8cf8885a621361d193
esraa237 <esraafoda2000@gmail.com>
2024-03-18 00:31:37 +0200

    finish task1 in lesson23-26 in week4

---
+++ week4/lesson23-26/task1/main.js
@@ -0,0 +1,14 @@
+// Your Solutions
+console.log(100_000); // 100000
+console.log(100000); // 100000
+console.log(1e5); // 100000
+console.log(5e4 + 5e4); // 100000
+console.log(5e4 * 2); // 100000
+console.log(2e5 / 2); // 100000
+console.log(2e5 - 1e5); // 100000
+console.log(1e2 ** 2.5); // 100000
+console.log(Math.pow(1e2, 2.5)); // 100000
+console.log(Math.round(99999.9)); // 100000
+console.log(Math.ceil(99999.6)); // 100000
+console.log(Math.trunc(100000.9)); // 100000
+console.log(Math.floor(100000.5)); // 100000
